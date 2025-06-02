fa58700c18b4fe3012c0b5a147fe5873dc0b6730
raisul227 <raisul2277@gmail.com>
2025-06-02 21:34:00 +0600

    make a responsive navigation bar

---
+++ app.js
@@ -0,0 +1,7 @@
+const sidebar = document.querySelector('.sidebar');
+const toggle= function(){
+    sidebar.style.display= 'block';
+}
+const eee = function(){
+    sidebar.style.display= 'none';
+}
